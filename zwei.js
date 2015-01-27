e7e7130ea65e348e46e78d42c5d7f43f0c847ffc
L3ve <370157506@qq.com>
2015-01-27 16:14:49 +0800

    移动工具方法到tool.js

--- zwei.js
+++ zwei.js
@@ -429,31 +429,4 @@
 
 }());
 
-//日期格式化    new Date().format("yyyy-MM-dd hh:mm:ss");
-Date.prototype.format = function (format) {
-    "use strict";
-    var k,
-        o = {
-            "M+": this.getMonth() + 1, //month
-            "d+": this.getDate(), //day
-            "h+": this.getHours(), //hour
-            "m+": this.getMinutes(), //minute
-            "s+": this.getSeconds(), //second
-            "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
-            "S": this.getMilliseconds() //millisecond
-        };
-    if (/(y+)/.test(format)) {
-        format = format.replace(RegExp.$1,
-            (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    }
-    for (k in o) {
-        if (new RegExp("(" + k + ")").test(format)) {
-            format = format.replace(RegExp.$1,
-                RegExp.$1.length == 1 ? o[k] :
-                    ("00" + o[k]).substr(("" + o[k]).length));
-        }
-    }
-    return format;
-};
-
 
